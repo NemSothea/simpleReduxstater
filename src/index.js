71f193ed53db10a741847e23b9536914695afb36
SotheaNem <nemsothe13@gmail.com>
2017-12-25 14:21:06 +0700

    generated youtube-api-key

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+const API_KEY = 'AIzaSyCZehA4Nroh86BbfyQENsSiEed6m42OxQs';
 
-import App from './components/app';
-import reducers from './reducers';
+//create new component. This component should produce
+ //some HTML
+const App = () => {
+	return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+ReactDOM.render(<App/>,document.querySelector('.container'));
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
