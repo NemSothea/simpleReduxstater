3cc0614160b4fa779473526f908b0832f08d8d31
nemsothea <nemsothea13@gmail.com>
2017-12-25 22:37:04 +0700

    video-detail error

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
 const API_KEY = 'AIzaSyCZehA4Nroh86BbfyQENsSiEed6m42OxQs';
 
 
@@ -20,6 +22,7 @@ class App extends Component {
 		return(
 		<div>
 			<SearchBar />
+			<VideoDetail video={this.state.videos[0]}/>
 			<VideoList videos={this.state.videos} />
 		</div>
 	);
